9460aa06b53735c288d4779338771516d87aa3b9
tungace <ductungnguyen1234@gmail.com>
2015-05-10 09:57:39 +0700

    Sua config

--- source/includes/config.php
+++ source/includes/config.php
@@ -6,8 +6,8 @@
 					/* DATABASE INFORMATION */ 
 
 					$mmhclass->info->config['sql_host']       = "localhost";
-					$mmhclass->info->config['sql_username']   = "the_millenials";
-					$mmhclass->info->config['sql_password']   = "123zxcv789";
+					$mmhclass->info->config['sql_username']   = "root";
+					$mmhclass->info->config['sql_password']   = "";
 					$mmhclass->info->config['sql_database']   = "the_millenials";
 					//$mmhclass->info->config['sql_tbl_prefix'] = "mmh_";
 
